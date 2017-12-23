0050fd0d7e16f288c04d625d174689728d4847f8
zzzzzga <1311949161@qq.com>
2017-12-23 17:59:46 +0800

    添加密码中断继续

--- addPasswordToDB.py
+++ addPasswordToDB.py
@@ -5,10 +5,14 @@ from databaseHelper import DatabaseHelper
 
 if __name__ == '__main__':
   dbHelper = DatabaseHelper('192.168.1.102', 'crack_ssh', 'root', '123456')
+  numCount = dbHelper.queryOne('select count(*) as num from password')['num']
   with open('password.txt', 'r') as f:
     for line in f:
-      try:
-        line = line.strip('\n').strip()
-        dbHelper.nonQuery('insert into password(val) value(%s)', (line,))
-      except Exception as e:
-        print(str(e))
+      if (numCount == 0):
+        try:
+          line = line.strip('\n').strip()
+          dbHelper.nonQuery('insert into password(val) value(%s)', (line,))
+        except Exception as e:
+          print(str(e))
+          exit()
+      numCount -= 1
